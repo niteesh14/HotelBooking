bfdbc4e4aab8fa59a0e5c4617ccab641ddf92166
niteesh14 <49438158+niteesh14@users.noreply.github.com>
2020-12-23 19:22:06 +0530

    Add files via upload

---
+++ Booking.java
@@ -0,0 +1,14 @@
+package hotelprogram;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Booking {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
